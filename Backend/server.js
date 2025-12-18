56a42da1936a24362cc8c6829de1471973ab15ca
Shashank Soni <shashanksoni815@gmail.com>
2025-12-18 11:51:18 +0530

    upload

--- Backend/server.js
+++ Backend/server.js
@@ -356,7 +356,7 @@ app.post('/api/patients/login', async (req, res) => {
 // ==========================
 // SERVER START
 // ==========================
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`🔥 Server running on http://127.0.0.1:${PORT}`);
 });
